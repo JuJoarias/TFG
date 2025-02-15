a3d50f57961f00146c01a3557fbc6593856e97e6
JuJoarias <juanjoseariasrojas@gmail.com>
2025-02-15 18:41:38 +0100

    grabable 47 (grab+slide)

--- hands_grabable.js
+++ hands_grabable.js
@@ -304,7 +304,7 @@ AFRAME.registerComponent('grabable', {
 
          const indexTipRight = manoDerecha.joints["index-finger-tip"];
          this.el.setAttribute('material', 'color', 'green');
-         this.reparent(indexTipRight);
+         this.reparent(manoDerecha);
 
       } else if (rightGrab && leftPinch) {
 
