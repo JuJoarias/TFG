641f7b555272bd71d7bf9e5a3f18a6ccf8024ad3
JuJoarias <juanjoseariasrojas@gmail.com>
2025-02-17 19:21:44 +0100

    grabable 60 (grab+slide)

--- hands_grabable.js
+++ hands_grabable.js
@@ -236,9 +236,12 @@ AFRAME.registerComponent('grabable', {
       const leftPinchState = manoIzquierda.pinchState;
       const Colide = this.isGrabbed;
 
-      document.querySelector('#text').setAttribute('text', `value: Colide: ${Colide}, Pinch derecha: ${rightPinchState}, Pinch izquierda: ${leftPinchState}`);
-      this.updateState(rightPinchState, Colide, leftPinchState, manoDerecha, manoIzquierda);
-      
+      if (rightPinchState !== this.lastPinchState || Colide !== this.lastGrabState || leftPinchState !== this.lastPinchState) {
+         document.querySelector('#text').setAttribute('text', `value: Colide: ${Colide}, Pinch derecha: ${rightPinchState}, Pinch izquierda: ${leftPinchState}`);
+         this.lastPinchState = rightPinchState;
+         this.lastGrabState = Colide;
+         this.updateState(rightPinchState, Colide, leftPinchState, manoDerecha, manoIzquierda);
+      }
    },
 
    updateState: function (rightPinch, rightGrab, leftPinch, manoDerecha, manoIzquierda) {
