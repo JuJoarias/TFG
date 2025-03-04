74ad05f81f3553364c4224cad4dd35619d3ba74f
JuJoarias <juanjoseariasrojas@gmail.com>
2025-03-04 20:37:14 +0100

    stretch test 7 mensajes debug

--- stretch.js
+++ stretch.js
@@ -432,11 +432,11 @@ AFRAME.registerComponent('stretch', {
    },
 
    tick: function () {
-      document.querySelector('#text').setAttribute('text', `value: dentro de tick. hand1: ${this.hand1} y hand2: ${this.hand2}`);
       if (this.hand1 && this.hand2) {
          const hand1Pos = this.hand1.object3D.position;
          const hand2Pos = this.hand2.object3D.position;
          const currentDistance = hand1Pos.distanceTo(hand2Pos);
+         document.querySelector('#text').setAttribute('text', `value: dentro de tick. hand1: ${this.hand1} y hand2: ${this.hand2} current distance: ${currentDistance} y previous distance: ${this.previousDistance}`);
 
          // 🔹 Solo modificar la escala si la distancia entre las manos ha cambiado
          if (Math.abs(currentDistance - this.previousDistance) > 0.01) {
