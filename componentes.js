1aeb3514998ac34e56001aec0c33b68878e694c5
JuJoarias <juanjoseariasrojas@gmail.com>
2025-03-05 20:39:59 +0100

    limpieza y renombre

--- componentes.js
+++ componentes.js
@@ -194,9 +194,6 @@ AFRAME.registerComponent('grabable', {
       this.rightHandEntity = document.querySelector('#right-hand');
       this.leftDetector = document.querySelector('#left-detector');
       this.rightDetector = document.querySelector('#right-detector');
-      this.lastGrabState = null;
-      this.isGrabbed = false;
-      this.hooverState = false;
       this.colideRight = false;
       this.colideLeft = false;
 
@@ -216,13 +213,10 @@ AFRAME.registerComponent('grabable', {
 
    check: function () {
       const manoDerecha = this.rightHandEntity.components.manos;
-      const detectorDerecho = this.rightDetector.components.detector;
       const manoIzquierda = this.leftHandEntity.components.manos;
-      const detectorIzquierdo = this.leftDetector.components.detector;
       this.collidingEntities = new Set();
 
       this.el.addEventListener('obbcollisionstarted', (evt) => {
-         this.isGrabbed = true;
          const otro = evt.detail.withEl.id
          if (otro.startsWith('Right')){
             this.colideRight = true;
@@ -241,7 +235,6 @@ AFRAME.registerComponent('grabable', {
          } else{
             this.colideLeft = false;
          }
-         this.isGrabbed = false;
          document.querySelector('#text2').setAttribute('text', `value: Colision con derecha: ${this.colideRight} Colision con izquierda: ${this.colideLeft}`);
       });
 
@@ -250,24 +243,13 @@ AFRAME.registerComponent('grabable', {
          return;
       }
 
-      if (!this.rightDetector || !detectorDerecho) {
-         document.querySelector('#text').setAttribute('text', `value: No se detecta el detector de la mano derecha`);
-         return;
-      }
-
       if (!this.leftHandEntity || !manoIzquierda) {
          document.querySelector('#text').setAttribute('text', `value: La mano izquierda no se detecta correctamente`);
          return;
       }
 
-      if (!this.leftDetector || !detectorIzquierdo) {
-         document.querySelector('#text').setAttribute('text', `value: No se detecta el detector de la mano izquierda`);
-         return;
-      }
-
       const rightPinchState = manoDerecha.pinchState;
       const leftPinchState = manoIzquierda.pinchState;
-      const Colide = this.isGrabbed;
       if (this.colideLeft || this.colideRight){
          this.el.emit('hooverStart');
       } else {this.el.emit('hooverEnd');}
@@ -283,7 +265,6 @@ AFRAME.registerComponent('grabable', {
       if ((this.colideRight || this.colideLeft ) && (rightPinch || leftPinch)) {
          if ((this.colideLeft && this.colideRight) && (rightPinch && leftPinch)) {
             this.el.setAttribute('material', 'color', 'black');
-            // this.reparent(this.el.sceneEl);
             this.el.emit('stretchStart', {hand1: indexTipLeft, hand2: indexTipRight})
             this.el.emit('slideEnd');
             this.el.emit('dragEnd');
@@ -294,8 +275,6 @@ AFRAME.registerComponent('grabable', {
             this.el.emit('dragEnd');
          } else if (this.colideRight && rightPinch){ 
             this.el.setAttribute('material', 'color', 'green');
-            // this.updateFakeCoords(manoDerecha);
-            // this.reparent(indexTipRight.object3D);
             this.el.emit('dragStart', {mano: manoDerecha, finger: indexTipRight})
             this.el.emit('stretchEnd');
             this.el.emit('slideEnd');
@@ -303,84 +282,11 @@ AFRAME.registerComponent('grabable', {
       
       } else {
          this.el.setAttribute('material', 'color', 'red');
-         // this.reparent(this.el.sceneEl);
          this.el.emit('stretchEnd');
          this.el.emit('slideEnd');
          this.el.emit('dragEnd');
       }
    },
-
-   // updateFakeCoords: function (mano) {
-   //    // Actualizar las posiciones de las esferas de la mano derecha
-   //    const pos1 = mano.joints["index-finger-metacarpal"].object3D.position;
-   //    const pos2 = mano.joints["index-finger-tip"].object3D.position;
-   //    const pos3 = mano.joints["pinky-finger-metacarpal"].object3D.position;
-
-   //    // Calcular el vector X (de esfera1 a esfera2)
-   //    this.vectorX.copy(pos2).sub(pos1).normalize();
-   //    this.fakeX.copy(this.vectorX);
-
-   //    // Calcular el vector Z (perpendicular al eje X, desde esfera3)
-   //    this.vectorXZ.copy(pos3).sub(pos1);
-   //    this.vectorZ.crossVectors(this.vectorX, this.vectorXZ).normalize();
-   //    this.fakeZ.copy(this.vectorZ);
-
-   //    // Calcular el eje Y como el producto cruzado entre X y Z
-   //    this.fakeY.crossVectors(this.fakeZ, this.fakeX).normalize();
-
-   //    // Asegurarnos de que el sistema sea ortonormal
-   //    if (this.fakeY.length() === 0) {
-   //       this.fakeZ.negate();
-   //       this.fakeY.crossVectors(this.fakeZ, this.fakeX).normalize();
-   //    }
-
-   //    // Definir la matriz de rotación usando los ejes falsos
-   //    this.rotationMatrix.makeBasis(this.fakeX, this.fakeY, this.fakeZ);
-
-   //    // Aplicar la rotación al cubo
-   //    this.el.object3D.setRotationFromMatrix(this.rotationMatrix);
-   // },
-
-   // reparent: function (newParent) {
-   //    const el = this.el;
-
-   //    // Verificar si el nuevo padre es un elemento DOM o un Object3D
-   //    if (!newParent) {
-   //       console.error('Nuevo padre no válido:', newParent);
-   //       return;
-   //    }
-
-   //    // Si el nuevo padre es un objeto DOM, tomamos su object3D
-   //    if (newParent instanceof HTMLElement) {
-   //       newParent = newParent.object3D;  // Convertimos el DOM a object3D
-   //    }
-
-   //    // Si el nuevo padre es un object3D, hacemos el reparenting
-   //    if (newParent instanceof THREE.Object3D) {
-   //       // Verificamos si ya es hijo del nuevo padre
-   //       if (el.object3D.parent === newParent) return;
-
-   //       // Guardar la posición global del cubo estático
-   //       const worldPosition = new THREE.Vector3();
-   //       worldPosition.setFromMatrixPosition(el.object3D.matrixWorld);
-
-   //       // Mover el object3D al nuevo padre
-   //       newParent.attach(el.object3D);
-
-   //       // Si el nuevo padre no es la escena, ajustar la posición local
-   //       if (newParent !== this.el.sceneEl.object3D) {
-   //          const localPosition = new THREE.Vector3();
-   //          localPosition.setFromMatrixPosition(newParent.matrixWorld).negate();
-   //          localPosition.add(worldPosition);
-   //          el.object3D.position.copy(localPosition);
-   //       } else {
-   //          // Si el nuevo padre es la escena, restaurar la posición original
-   //          el.object3D.position.copy(worldPosition);
-   //       }
-   //    } else {
-   //       console.error('Nuevo padre debe ser un HTMLElement o un Object3D.');
-   //    }
-   // },
 });
 
 AFRAME.registerComponent('drag', {
